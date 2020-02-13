06e6827dfaa6400af77d3fda3445822287aa90da
Otmane Sabir <o.sabir@jacobs-university.de>
2020-02-13 16:09:57 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/ImgProc_HW1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ImgProc_HW1.dir/main.cpp.o"
+  "ImgProc_HW1"
+  "ImgProc_HW1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ImgProc_HW1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
