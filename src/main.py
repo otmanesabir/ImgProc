1a12e697b13615b4c928961bed4aa4028f71e9b9
Aadil Anil Kumar <aadilanilkumar@Aadils-MBP.wlan.jacobs-university.de>
2020-02-18 14:21:20 +0100

    ++dilation

--- src/main.py
+++ src/main.py
@@ -5,14 +5,14 @@ import numpy as np
 def getParams(argss):
     op = 0
     # e = 1, d = 2
-    if len(argss) < 5:
+    if len(argss) < 5 or argv[1] not in ('e', 'd'):
         print("usage: e/d <SE file> <input file> <output file>")
         exit()
 
     if argv[1] == 'e':
-        operation = 1
+        op = 1
     elif argv[1] == 'd':
-        operation = 2
+        op = 2
 
     se = "../input/" + argv[2]
     infile = "../input/" + argv[3]
@@ -37,14 +37,13 @@ def erosion(matrix, se):
             ans[i][j] = min_val(matrix, i, j, se)
     print(ans)
 
-
 def min_val(matrix, a, b, se):
     i = a
     j = b
     mini_val = 1
     for x in range(len(se)):
         for y in range(len(se[0])):
-            if 0 <= b < len(matrix[0]) - 1:
+            if 0 <= b < len(matrix[0]):
                 if matrix[a][b] < mini_val:
                     mini_val = matrix[a][b]
                 b += 1
@@ -57,6 +56,39 @@ def min_val(matrix, a, b, se):
             break
     return mini_val
 
+# DILATION USING MAX - BINARY IMAGES
+# The value of the output pixel is the max value of the pixels that are in 
+# neighborhood the size of the SE within the input image.
+# max_val finds the largest element in the sub-matrix
+
+def dilation(matrix, se):
+    ans = np.zeros((len(matrix), len(matrix[0])))
+    for i in range(len(matrix)): 
+        for j in range(len(matrix[0])):
+            ans[i][j] = max_val(matrix, i, j, se)
+    print(ans)
+            
+def max_val(matrix, a, b, se): 
+    i = a
+    j = b
+    max_val =  1
+    for x in range(len(se)): 
+        for y in range(len(se[0])):
+            # print(max_val, i, j)
+            if 0 <= b < len(matrix[0]):
+                if matrix[i][j] > max_val: 
+                    max_val = matrix[i][j]
+                b += 1
+            else:
+                # max_val = 0
+                break
+        if 0 <= a < len(matrix) - 1: 
+            a += 1
+            b = j
+        else:
+            break   
+    return max_val
+                        
 
 def main():
     operation, s, infile, outfile = getParams(argv)
@@ -64,14 +96,18 @@ def main():
     se = open(s, "r")
     matrix = genMatrix(f)
     se_matrix = genMatrix(se)
+
     # view in matrix format
     print("Original")
     for row in matrix:
         print(' '.join(map(str, row)))
-    print("Erosion :")
-    erosion(matrix, se_matrix)
-    # print(se_matrix)
 
+    if operation == 1:
+        print("Erosion:")
+        erosion(matrix, se_matrix)
+    else:
+        print("Dilation:")
+        dilation(matrix, se_matrix)
 
 if __name__ == '__main__':
     main()
