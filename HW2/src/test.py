42f3b1dea2888014eca3d51ca19db9143e0802d9
Otmane Sabir <o.sabir@jacobs-university.de>
2020-04-03 23:20:24 +0200

    bunch of stuff :tada:

--- HW2/src/test.py
+++ HW2/src/test.py
@@ -4,134 +4,172 @@ import matplotlib.pyplot as plt
 import threading
 from tqdm import tqdm
 from collections import deque
-from ipython_genutils.py3compat import xrange
+from PIL import Image
+import imageio
+import matplotlib.pyplot as plt
+import pandas as pd
+from progressbar import ProgressBar
 
 
-class Watershed(object):
-    MASK = -2
-    WSHD = 0
-    INIT = -1
-    INQE = -3
+MASK = -2
+WSHD = 0
+INIT = -1
+INQUEUE = -3
+LVLS = 256
 
-    def __init__(self, levels=256):
-        self.levels = levels
+def genMatrix(filename):
+    with open(filename, 'r') as f:
+        l = [[int(num) for num in line.split(',')] for line in f]
+    imageio.imwrite('./temp.png', l)
+    return l
 
-    # Neighbour (coordinates of) pixels, including the given pixel.
-    def _get_neighbors(self, height, width, pixel):
-        # MATRIX HEIGHT
-        i = max(0, pixel[0] - 1)
-        j = min(height, pixel[0] + 1)
 
-        # MATRIX WIDTH
-        x = max(0, pixel[1] - 1)
-        y = min(width, pixel[1] + 1)
-        # x = 2
-        # y = 5
+def getNeighbors(height, width, pixel, n):
+    i = max(0, pixel[0] - 1)
+    j = min(height, pixel[0] + int(n / 4))
 
-        matrix = np.zeros(shape=(j - i, y - x))
-        for t in range(j - i):
-            temp = np.full((1, (y - x)), t + i)
-            matrix[t] = temp
-        temp = np.arange(x, y)
-        matrix2 = np.zeros(shape=(j - i, y - x))
-        for t in range(y - x):
-            if t == j - i: break
-            matrix2[t] = temp
-        glob = np.zeros((2, j - i, y - x))
-        glob[0] = matrix
-        glob[1] = matrix2
-        return glob.astype('int64').reshape(2, -1).T
+    x = max(0, pixel[1] - 1)
+    y = min(width, pixel[1] + int(n / 4))
 
-    def apply(self, image):
-        current_label = 0
-        flag = False
-        fifo = deque()
+    matrix = np.zeros(shape=(j - i, y - x))
+    for t in range(j - i):
+        temp = np.full((1, (y - x)), t + i)
+        matrix[t] = temp
 
-        height, width = image.shape
+    temp = np.arange(x, y)
+    matrix2 = np.zeros(shape=(j - i, y - x))
+    for t in range(y - x):
+        if t == j - i: break
+        matrix2[t] = temp
+    glob = np.zeros((2, j - i, y - x))
+    glob[0] = matrix
+    glob[1] = matrix2
+
+    return glob.reshape(2, -1).T.astype(int)
+
+
+def getPixels(height, width):
+    y_axis = np.zeros((height, width))
+    for j in range(height):
+        row = np.full((1, width), j)
+        y_axis[j] = row
+
+    x_axis = np.zeros((height, width))
+    try:
+        row = np.arange(0, height)
+        for i in range(height):
+            x_axis[i] = row
+    except:
+        try:
+            row = np.arange(0, height + 1)
+            for i in range(height):
+                x_axis[i] = row
+        except:
+            row = np.arange(0, width)
+            for i in range(height):
+                x_axis[i] = row
+
+    plane = np.zeros((2, height, width))
+    plane[0] = y_axis
+    plane[1] = x_axis
+
+    return plane.reshape(2, -1).T.astype(int)
+
+
+def watershed(img, n):
+    pbar1 = ProgressBar()
+    pbar2 = ProgressBar()
+    current_label = 0
+    flag = False
+    que = deque()
+    try:
+        height, width = img.shape
         total = height * width
-        labels = np.full((height, width), self.INIT, np.int32)
+    except:
+        try:
+            img.transpose(2, 0, 1).reshape(3, -1)
+            height, width = img.shape
+            total = height * width
+        except:
+            print("Image format not supported")
+            exit()
 
-        reshaped_image = image.reshape(total)
-        # [y, x] pairs of pixel coordinates of the flattened image.
-        pixels = np.mgrid[0:height, 0:width].reshape(2, -1).T
-        # Coordinates of neighbour pixels for each pixel.
-        neighbours = np.array([self._get_neighbors(height, width, p) for p in pixels])
-        if len(neighbours.shape) == 3:
-            # Case where all pixels have the same number of neighbours.
-            neighbours = neighbours.reshape(height, width, -1, 2)
-        else:
-            # Case where pixels may have a different number of pixels.
-            neighbours = neighbours.reshape(height, width)
+    labels = np.full((height, width), INIT, np.int32)  # Flat output image matrix, initialized with INIT
+    flat_img = img.reshape(total)  # Flattening the image
+    pixels = getPixels(height, width)  # Getting [y, x] pairs pairs of image
+    print("Getting {} Neighbours...".format(n))
+    neighbours = np.array(
+        [getNeighbors(height, width, p, n) for p in pbar1(pixels)])  # Getting [y, x] pairs for neighbours of all pixels
+    neighbours = neighbours.reshape(height, width)
 
-        indices = np.argsort(reshaped_image)
-        sorted_image = reshaped_image[indices]
-        sorted_pixels = pixels[indices]
+    # Sorting pixels direct access
+    idx = np.argsort(flat_img)
+    sorted_img = flat_img[idx]
+    sorted_pixels = pixels[idx]
 
-        # self.levels evenly spaced steps from minimum to maximum.
-        levels = np.linspace(sorted_image[0], sorted_image[-1], self.levels)
-        level_indices = []
-        current_level = 0
+    # Creating 256 evenly spaced grey value levels
+    lvls = np.linspace(sorted_img[0], sorted_img[-1], LVLS)
+    lvl_idx = []
+    current_lvl = 0
 
-        # Get the indices that deleimit pixels with different values.
-        for i in range(total):
-            if sorted_image[i] > levels[current_level]:
-                # Skip levels until the next highest one is reached.
-                while sorted_image[i] > levels[current_level]: current_level += 1
-                level_indices.append(i)
-        level_indices.append(total)
+    # Getting indices of pixels that have diff grey value levels
+    for i in range(total):
+        if sorted_img[i] > lvls[current_lvl]:
+            while sorted_img[i] > lvls[current_lvl]:
+                current_lvl += 1
+            lvl_idx.append(i)
+    lvl_idx.append(total)
 
-        start_index = 0
-        for stop_index in level_indices:
-            # Mask all pixels at the current level.
-            for p in sorted_pixels[start_index:stop_index]:
-                labels[p[0], p[1]] = self.MASK
-                # Initialize queue with neighbours of existing basins at the current level.
-                for q in neighbours[p[0], p[1]]:
-                    # p == q is ignored here because labels[p] < WSHD
-                    if labels[q[0], q[1]] >= self.WSHD:
-                        labels[p[0], p[1]] = self.INQE
-                        fifo.append(p)
-                        break
+    print("Creating Segmented Image...")
+    start = 0
+    for stop in pbar2(lvl_idx):
 
-            # Extend basins.
-            while fifo:
-                p = fifo.popleft()
-                # Label p by inspecting neighbours.
-                for q in neighbours[p[0], p[1]]:
-                    # Don't set lab_p in the outer loop because it may change.
-                    lab_p = labels[p[0], p[1]]
-                    lab_q = labels[q[0], q[1]]
-                    if lab_q > 0:
-                        if lab_p == self.INQE or (lab_p == self.WSHD and flag):
-                            labels[p[0], p[1]] = lab_q
-                        elif lab_p > 0 and lab_p != lab_q:
-                            labels[p[0], p[1]] = self.WSHD
-                            flag = False
-                    elif lab_q == self.WSHD:
-                        if lab_p == self.INQE:
-                            labels[p[0], p[1]] = self.WSHD
-                            flag = True
-                    elif lab_q == self.MASK:
-                        labels[q[0], q[1]] = self.INQE
-                        fifo.append(q)
+        # Masking all pixels at current level
+        for pixel in sorted_pixels[start:stop]:
+            labels[pixel[0], pixel[1]] = MASK
+            # Adding the neighbours of existing basins to queue
+            for nbr_pixel in neighbours[pixel[0], pixel[1]]:
+                if labels[nbr_pixel[0], nbr_pixel[1]] >= WSHD:
+                    labels[pixel[0], pixel[1]] = INQUEUE
+                    que.append(pixel)
+                    break
 
-            # Detect and process new minima at the current level.
-            for p in sorted_pixels[start_index:stop_index]:
-                # p is inside a new minimum. Create a new label.
-                if labels[p[0], p[1]] == self.MASK:
-                    current_label += 1
-                    fifo.append(p)
-                    labels[p[0], p[1]] = current_label
-                    while fifo:
-                        q = fifo.popleft()
-                        for r in neighbours[q[0], q[1]]:
-                            if labels[r[0], r[1]] == self.MASK:
-                                fifo.append(r)
-                                labels[r[0], r[1]] = current_label
+        # Extending basis
+        while que:
+            pixel = que.popleft()
+            for nbr_pixel in neighbours[pixel[0], pixel[1]]:
+                pixel_label = labels[pixel[0], pixel[1]]
+                nbr_label = labels[nbr_pixel[0], nbr_pixel[1]]
+                if nbr_label > 0:
+                    if pixel_label == INQUEUE or (pixel_label == WSHD and flag):
+                        labels[pixel[0], pixel[1]] = nbr_label
+                    elif pixel_label > 0 and pixel_label != nbr_label:
+                        labels[pixel[0], pixel[1]] = WSHD
+                        flag = False
+                elif nbr_label == WSHD:
+                    if pixel_label == INQUEUE:
+                        labels[pixel[0], pixel[1]] = WSHD
+                        flag = True
+                elif nbr_label == MASK:
+                    labels[nbr_pixel[0], nbr_pixel[1]] = INQUEUE
+                    que.append(nbr_pixel)
 
-            start_index = stop_index
+        # Looking for new minimas
+        for pixel in sorted_pixels[start:stop]:
+            if labels[pixel[0], pixel[1]] == MASK:
+                current_label += 1
+                que.append(pixel)
+                labels[pixel[0], pixel[1]] = current_label
+                while que:
+                    q = que.popleft()
+                    for r in neighbours[q[0], q[1]]:
+                        if labels[r[0], r[1]] == MASK:
+                            que.append(r)
+                            labels[r[0], r[1]] = current_label
 
-        return labels
+        start = stop
+
+    return labels
 
 
 def makePlot(x, y):
@@ -150,12 +188,11 @@ def makePlot(x, y):
     plt.legend()
     plt.show()
 
-def test_single(i):
-    w = Watershed()
+def test_single(i, n):
     image = np.round(np.random.rand(i, i) * 255)
     height, width = image.shape
     start = timeit.default_timer()
-    w.apply(image)
+    watershed(image, n)
     stop = timeit.default_timer()
     x.append([height*width, stop - start])
     return
@@ -165,11 +202,11 @@ x = [[]]
 
 # MULTI THREADING TEST (FASTER)
 
-def test_mtt():
+def test_mtt(n):
     i = 1
     threads = []
-    for _ in tqdm(range(1000), desc="creating threads"):
-        t = threading.Thread(target=test_single, args=(i,))
+    for _ in tqdm(range(300), desc="creating threads"):
+        t = threading.Thread(target=test_single, args=(i, n))
         threads.append(t)
         i += 1
 
@@ -193,25 +230,46 @@ def test_mtt():
 
 
 # RANDOM SIMPLE TEST
-def test_rnd():
-    w = Watershed()
+def test_rnd(n):
     x = []
     y = []
     i = 1
-    for _ in tqdm(range(1000), desc="Watershed PT"):
+    for _ in tqdm(range(300), desc="Watershed PT"):
         image = np.round(np.random.rand(i, i) * 255)
         x.append(image.size)
         start = timeit.default_timer()
-        w.apply(image)
+        watershed(image, n)
         stop = timeit.default_timer()
         y.append(float(stop - start))
         i += 1
     makePlot(x, y)
 
 
+def barChart():
+    top = []
+    i = 2
+    image = np.round(np.random.rand(512, 512) * 255)
+    while(i < 128):
+        start = timeit.default_timer()
+        watershed(image, i)
+        stop = timeit.default_timer()
+        top.append((i, (stop - start)))
+        i *= 2
+    labels, ys = zip(*top)
+    xs = np.arange(len(labels))
+    width = 0.5
+    plt.bar(xs, ys, width, align='center', color=['#38B6FF', '#ED254E', '#9FFFCB', '#7B4B94', '#7D82B8', '#2E5266', '#545454'])
+    plt.xticks(xs, labels)  # Replace default x-ticks with xs, then replace xs with labels
+    plt.yticks(ys)
+    plt.xlabel('Number of Neighbors', fontsize=12)
+    plt.ylabel('Time [S]', fontsize=12)
+    plt.savefig('neighbor-barchart.png')
+
 if __name__ == "__main__":
+    test_rnd(8)
     #test_mtt()
+    #barChart()
     #test_img()
-    test_rnd()
+
 
 
