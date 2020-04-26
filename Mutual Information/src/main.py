33545266aabd0cc6d0a47129acd08122bf74bbe4
Otmane Sabir <o.sabir@jacobs-university.de>
2020-04-26 09:15:17 +0200

    refactors

--- Mutual Information/src/main.py
+++ Mutual Information/src/main.py
@@ -1,39 +1,69 @@
+import matplotlib
 import numpy as np
-import math
-import imageio
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import entropy as scipy_entropy
 from PIL import Image
-from tqdm import tqdm
+
+plt.style.use('seaborn')
+
+def splitImage(img):
+    split_img = Image.Image.split(img)
+    return split_img[0], split_img[1], split_img[2]
+
+def cropImage(img):
+    r, g, b = splitImage(img)
+    w, h = img.size
+    top = r.crop((20, 0, w-20, h))
+    return top, [g.crop((40-x, 0, w-x, h)) for x in range(40, -1, -1)]
+
+def shannon_entropy(image, base=2):
+    _, counts = np.unique(image, return_counts=True)
+    return scipy_entropy(counts, base=base)
+
+def entropyCalc(X):
+    uniq = set(X)
+    P = [np.mean(X == x) for x in uniq]
+    return sum(-p * np.log2(p) for p in P)
 
 
-def findHistogram(img):
-    # filled soon
+def mutualInfoCalc(img, num_bins):
+    top, bottoms = cropImage(img)
+    mis = []
+    for i in range(len(bottoms)):
+        hist = np.histogram2d(np.asarray(top).flatten(), np.asarray(bottoms[i]).flatten(), bins=num_bins)
+        hist_img = Image.fromarray(hist[0], 'RGB')
+        mis.append(entropyCalc(np.asarray(top).flatten()) + entropyCalc(np.asarray(bottoms[i]).flatten()) - entropyCalc(np.asarray(hist_img).flatten()))
+    return mis
+
+def binSizeChange(img):
+    top, bottoms = cropImage(img)
+    mis = []
+    for i in range(1, 256):
+        hist = np.histogram2d(np.asarray(top).flatten(), np.asarray(bottoms[20]).flatten(), bins=int(256/i))
+        hist_img = Image.fromarray(hist[0], 'RGB')
+        mis.append(entropyCalc(np.asarray(top).flatten()) + entropyCalc(np.asarray(bottoms[20]).flatten()) - entropyCalc(np.asarray(hist_img).flatten()))
+    return mis
 
 
-def getImg(path):
-    img = np.array(Image.open(path))
-    return img
+def mutualInformationTests():
+    flower = Image.open("../input/flower.png")
+    fig = plt.figure()
+    plt.ylabel('Mutual Information')
+    plt.xlabel('Image Translations')
+    sns.lineplot([x for x in range(41)], mutualInfoCalc(flower, 256))
+    plt.savefig("../output/flower_translations")
+    puffin = Image.open("../input/puffin.jpg")
+    fig = plt.figure()
+    plt.ylabel('Mutual Information')
+    plt.xlabel('Image Translations')
+    sns.lineplot([x for x in range(41)], mutualInfoCalc(puffin, 256))
+    plt.savefig("../output/puffin_translations")
 
-def draft():
-    #the draft we made the other day.
-    mi = 0.0
-    jointD = [[0.2, 0.1, 0.2],
-        [0, 0.2, 0.1],
-        [0.1, 0, 0.1]]
-    mdX = [0.3, 0.3, 0.4]
-    mdY = [0.5, 0.3, 0.2]
-    for i in range(3):
-        for j in range(3):
-            print(i)
-            if jointD[i][j] == 0:
-                mi += 0
-            else:
-                a = mdX[j]
-                b = mdY[i]
-                mi += jointD[i][j]*math.log2(jointD[i][j]/(mdX[j]*mdY[i]))
-    print(mi)
 
 def main():
-    findHistogram(getImg("../input/nuclei.png"))
+    mutualInformationTests()
+
 
 
 if __name__ == '__main__':
