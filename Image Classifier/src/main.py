487edc0a668afa3ef6a350fe23128e378ec99d5f
Otmane Sabir <o.sabir@jacobs-university.de>
2020-05-04 13:01:23 +0200

    added lfs for better handling

--- Image Classifier/src/main.py
+++ Image Classifier/src/main.py
@@ -1,8 +1,9 @@
-import numpy as np
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
 
 def main():
-    print("Hello")
+    print("Hello world")
 
 if __name__ == "__main__":
     main()
